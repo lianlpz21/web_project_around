6410ad196aa762f5ff784a755bfd3bf7bea6542a
lianlpz21 <lian.lpz21@gmail.com>
2024-03-22 17:21:18 -0300

    popup with overlay added

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,16 @@
 const popup = document.querySelector(".popup");
 const popupOpen = document.querySelector(".profile__edit-btn");
 const popupClose = document.querySelector(".popup__icon");
-const body = document.querySelector(".body");
+const overlay = document.querySelector(".overlay");
 
 popupOpen.addEventListener("click", function () {
   popup.classList.add("popup_opened");
-  body.classList.add("body_popup-bg");
+  overlay.classList.add("overlay_opened");
+  // overlay.style.display = "block"; // Mostrar el overlay cuando se abre el popup
 });
 
 popupClose.addEventListener("click", () => {
   popup.classList.remove("popup_opened");
-  body.classList.remove("body_popup-bg");
+  overlay.classList.remove("overlay_opened");
+  // overlay.style.display = "none";
 });
