5dbaaef955d9e8bc654d8842c99d31196d0ac2ab
lianlpz21 <lian.lpz21@gmail.com>
2024-03-23 17:00:53 -0300

    Abrir formulario, cerrar y ingresar datos del form al perfil

--- scripts/index.js
+++ scripts/index.js
@@ -3,14 +3,35 @@ const popupOpen = document.querySelector(".profile__edit-btn");
 const popupClose = document.querySelector(".popup__icon");
 const overlay = document.querySelector(".overlay");
 
+const form = document.querySelector("#form");
+const inputName = document.querySelector("#input__name");
+const inputJob = document.querySelector("#input__job");
+const profileName = document.querySelector(".profile__name");
+const jobName = document.querySelector(".profile__occupation");
+
 popupOpen.addEventListener("click", function () {
   popup.classList.add("popup_opened");
   overlay.classList.add("overlay_opened");
-  // overlay.style.display = "block"; // Mostrar el overlay cuando se abre el popup
 });
 
 popupClose.addEventListener("click", () => {
   popup.classList.remove("popup_opened");
   overlay.classList.remove("overlay_opened");
-  // overlay.style.display = "none";
 });
+
+const btn = document.querySelector(".popup__btn");
+
+btn.addEventListener("click", () => {
+  popup.classList.remove("popup_opened");
+  overlay.classList.remove("overlay_opened");
+});
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  let nameOutput = inputName.value;
+  let jobOutput = inputJob.value;
+  profileName.textContent = nameOutput;
+  jobName.textContent = jobOutput;
+}
+form.addEventListener("submit", handleProfileFormSubmit);
