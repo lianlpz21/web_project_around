e1b098ad74d3562d033e8b4ba4d40c80efbe3754
lianlpz21 <lian.lpz21@gmail.com>
2024-05-10 19:32:08 -0400

    adding modal section

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,44 @@
-const popup = document.querySelector(".popup");
+// const popup = document.querySelector(".popup");
 const profileEditBtn = document.querySelector(".profile__edit-btn");
-const closeBtn = document.querySelector(".popup__icon");
+const addBtn = document.querySelector(".profile__add-btn");
+const closeBtn = document.querySelectorAll(".popup__icon");
+
+const editFormOpener = document.querySelector("#form__edit-opener");
+const addFormOpener = document.querySelector("#form__add-cards-opener");
+
 const overlay = document.querySelector(".overlay");
-const form = document.querySelector("#form");
 const inputName = document.querySelector("#input__name");
 const inputJob = document.querySelector("#input__job");
 const profileName = document.querySelector(".profile__name");
 const profileJob = document.querySelector(".profile__occupation");
 const saveBtn = document.querySelector(".popup__btn");
 
-// const profileAddButton = document.querySelector(".profile__add-btn");
-
-function formPopupAdder() {
+function formPopupAdder(popup) {
   popup.classList.add("popup_opened");
   overlay.classList.add("overlay_opened");
 }
 
-function formPopupRemover() {
+function formPopupRemover(popup) {
   popup.classList.remove("popup_opened");
   overlay.classList.remove("overlay_opened");
 }
 
 profileEditBtn.addEventListener("click", function () {
-  formPopupAdder();
+  formPopupAdder(editFormOpener);
 });
 
-closeBtn.addEventListener("click", () => {
-  formPopupRemover();
+addBtn.addEventListener("click", () => {
+  formPopupAdder(addFormOpener);
 });
 
-// profileAddButton.addEventListener("click", () => {
-//   // console.log("click");
-//   formPopupAdder();
-// });
+closeBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    formPopupRemover(editFormOpener);
+    formPopupRemover(addFormOpener);
+  });
+});
 
+//* Sending personal info in form
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
 
@@ -42,9 +47,14 @@ function handleProfileFormSubmit(evt) {
   profileName.textContent = nameOutput;
   profileJob.textContent = jobOutput;
 
-  formPopupRemover();
+  if (nameOutput === "" && jobOutput === "") {
+    profileName.textContent = "Jacques Cousteau";
+    profileJob.textContent = "Explorador";
+  }
+
+  formPopupRemover(editFormOpener);
 }
-form.addEventListener("submit", handleProfileFormSubmit);
+editFormOpener.addEventListener("submit", handleProfileFormSubmit);
 
 //* Initial cards in a array
 const initialCards = [
@@ -74,7 +84,7 @@ const initialCards = [
   },
 ];
 
-//* Adding initial cards
+//* Adding initial cards and like button
 const cards = document.querySelector(".cards");
 const template = document.querySelector("#template").content;
 
@@ -82,16 +92,65 @@ initialCards.forEach((card) => {
   const copy = template.cloneNode(true);
   const cardImage = copy.querySelector(".card__image");
   const cardTitle = copy.querySelector(".card__title");
+  const likeBtn = copy.querySelector(".card__like");
   cardImage.src = card.link;
   cardTitle.textContent = card.name;
+
+  //* Like button
+  likeBtn.addEventListener("click", () => {
+    likeBtn.classList.toggle("card__liked");
+  });
+
+  //* Delete button
+  const deleteCardBtn = copy.querySelector(".card__trash");
+  deleteCardBtn.addEventListener("click", () => {
+    deleteCardBtn.parentElement.remove();
+  });
+
+  //* Bigger image
 
   cards.appendChild(copy);
 });
 
-//* Like button
-const likeBtns = document.querySelectorAll(".card__like");
-likeBtns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    btn.classList.toggle("card__liked");
+//* Adding new cards sended by form
+addFormOpener.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+
+  const copy = template.cloneNode(true);
+  const cardImage = copy.querySelector(".card__image");
+  const cardTitle = copy.querySelector(".card__title");
+  const inputImage = document.querySelector("#input__image");
+  const inputTitle = document.querySelector("#input__title");
+  cardImage.src = inputImage.value;
+  cardTitle.textContent = inputTitle.value;
+
+  formPopupRemover(addFormOpener);
+
+  const deleteCardBtn = copy.querySelector(".card__trash");
+  deleteCardBtn.addEventListener("click", () => {
+    deleteCardBtn.parentElement.remove();
   });
+
+  cards.appendChild(copy);
+
+  inputTitle.value = "";
+  inputImage.value = "";
 });
+
+//* Get bigger image after clicking
+// const card = document.querySelectorAll(".card");
+// const modal = document.querySelector("#modal");
+// const modalImage = modal.querySelector(".modal__image");
+
+// card.forEach((card) => {
+//   card.addEventListener("click", (evt) => {
+//     const clickedImage = evt.currentTarget;
+//     const image = clickedImage.querySelector(".card__image");
+//     const copy = template.cloneNode(true);
+//     const cardImage = copy.querySelector(".card__image");
+//     const openModal = modal.show(copy);
+
+//     clickedImage.value = cardImage.src;
+//     modal.appendChild(copy);
+//   });
+// });
