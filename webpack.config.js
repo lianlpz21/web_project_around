164894a2c9dca10c76f0ef2719a3e4a52538270f
lianlpz21 <lian.lpz21@gmail.com>
2024-06-27 19:30:35 -0400

    proyecto completo minificado

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
@@ -34,7 +35,9 @@ module.exports = {
           MiniCssExtractPlugin.loader,
           {
             loader: "css-loader",
+            options: { importLoaders: 1 },
           },
+          "postcss-loader",
         ],
       },
       {
@@ -48,6 +51,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/index.html",
     }),
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
   ],
 };
